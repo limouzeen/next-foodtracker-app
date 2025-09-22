f168ce28e5c68bf67832effc19c1b85d77cb2156
limouzeen <amaratkos@hotmail.com>
2025-09-22 17:44:47 +0700

    Finished

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+export const dynamic = "force-dynamic";
+
+
+
 import { useMemo, useRef, useState } from 'react';
 import Link from 'next/link';
 import Image, { StaticImageData } from 'next/image';
