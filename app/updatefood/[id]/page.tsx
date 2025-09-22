a6db32acd390d003f658b1e81e93d2fd19f41451
limouzeen <amaratkos@hotmail.com>
2025-09-22 16:58:51 +0700

    fix

--- app/updatefood/[id]/page.tsx
+++ app/updatefood/[id]/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+
 import { useEffect, useMemo, useRef, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
